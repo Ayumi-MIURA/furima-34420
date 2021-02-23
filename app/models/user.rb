9183d79f26ec9127fd8a32aa6c859aa3ec13e3f3
Ayumi-MIURA <78206576+Ayumi-MIURA@users.noreply.github.com>
2021-02-23 17:12:47 +0900

    準備＆バリデーション設定

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birthday, presence: true  
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+       
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+        
+  with_options presence: true, format: { with:/\A[ァ-ヶ]+\z/} do
+    validates :first_name_katakana
+    validates :last_name_katakana
+  end
+       
+  has_many   :items
+
+end
