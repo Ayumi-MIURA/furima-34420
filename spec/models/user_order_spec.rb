776f629cc400858de0c2cf3c930afb2c1e4054b4
Ayumi-MIURA <78206576+Ayumi-MIURA@users.noreply.github.com>
2021-03-02 21:49:24 +0900

    修正3

--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UserOrder, type: :model do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
-    @user_order = FactoryBot.build(:user_order, user_id: @user.id , item_id: @item.id)
+    @user_order = FactoryBot.build(:user_order, user_id: @user.id, item_id: @item.id)
     sleep 0.1
   end
 
